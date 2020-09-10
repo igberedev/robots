0b9e4cbcae24d4125e30fd87cee83ccb0736344f
Agwara <nnaemekaagwara@gmail.com>
2020-09-10 14:06:38 +0100

    Updates

---
+++ precache-manifest.7c4fa490db8da5964134cce3ce62ca58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b2a39ae7ee568d9dd5afd146bad542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "094e333aa6c974221dc7",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ec862cf9de818d1bbb9f",
+    "url": "/static/css/main.ddec0faf.chunk.css"
+  },
+  {
+    "revision": "094e333aa6c974221dc7",
+    "url": "/static/js/2.c02e5628.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c02e5628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec862cf9de818d1bbb9f",
+    "url": "/static/js/main.afe85258.chunk.js"
+  },
+  {
+    "revision": "6be05cda1f2a19f53a1d",
+    "url": "/static/js/runtime-main.1b6c883f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
